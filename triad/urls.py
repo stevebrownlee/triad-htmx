22429b7575b746923bd12c88fcac2b8b09512484
Steve Brownlee <chortlehoort@gmail.com>
2023-12-24 21:01:16 -0600

    Got PUT to work on ingredients

--- triad/urls.py
+++ triad/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('ingredients', ingredient_list, name='ingredient-list'),
     path('ingredients/<int:ingredient_id>', ingredient_details, name="ingredient"),
     path('ingredients/form', ingredient_form, name='ingredient_form'),
+    path('ingredients/<int:ingredient_id>/form', ingredient_form, name='ingredient_form'),
 
     # path('logout', logout_user, name='logout'),
     # path('register', register_user, name='register'),
--- triad/views/ingredients.py
+++ triad/views/ingredients.py
@@ -1,3 +1,4 @@
+from django.http import QueryDict
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
@@ -9,14 +10,30 @@ def ingredients(request):
     return render(request, 'ingredients/index.html')
 
 @require_http_methods(['GET'])
-def ingredient_form(request):
+def ingredient_form(request, ingredient_id=None):
     types = Type.objects.all()
-    return render(request, 'ingredients/partials/_form.html', {'ingredient': None, 'types': types})
 
-@require_http_methods(['GET'])
+    ingredient = None
+    if ingredient_id is not None:
+        ingredient = Ingredient.objects.get(pk=ingredient_id)
+
+    return render(request, 'ingredients/partials/_form.html', {'ingredient': ingredient, 'types': types})
+
+@require_http_methods(['GET', 'PUT'])
 def ingredient_details(request, ingredient_id):
-    ingredient= Ingredient.objects.get(pk=ingredient_id)
-    return render(request, 'ingredients/partials/_detail.html', {'ingredient': ingredient})
+    ingredient = Ingredient.objects.get(pk=ingredient_id)
+
+    if request.method == 'GET':
+        return render(request, 'ingredients/partials/_detail.html', {'ingredient': ingredient})
+    elif request.method == 'PUT':
+        data = QueryDict(request.body)
+
+        ingredient.name = data.get("name")
+        ingredient.potency = data.get("potency")
+        ingredient.type_id = data.get("type")
+        ingredient.save()
+
+        return render(request, 'ingredients/partials/_detail.html', {'ingredient': ingredient})
 
 @require_http_methods(['GET', 'POST'])
 def ingredient_list(request):
